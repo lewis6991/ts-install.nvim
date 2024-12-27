e6ea285d54c22e1652e03754bd693aeb57035297
Lewis Russell <lewis6991@gmail.com>
2024-12-27 16:32:46 +0000

    chore: update async lib

--- lua/ts-install/async.lua
+++ lua/ts-install/async.lua
@@ -2,40 +2,89 @@ local M = {}
 
 --- Executes a future with a callback when it is done
 --- @param func function
---- @param callback function
+--- @param protected boolean
+--- @param callback? function
 --- @param ... unknown
-local function execute(func, callback, ...)
-  local thread = coroutine.create(func)
+local function run(func, protected, callback, ...)
+  local co = coroutine.create(func)
+
+  if protected then
+    assert(type(callback) == 'function')
+  end
 
   local function step(...)
-    local ret = { coroutine.resume(thread, ...) }
-    --- @type boolean, any
-    local stat, nargs_or_err = unpack(ret)
+    local ret = { coroutine.resume(co, ...) }
+    local stat = ret[1]
 
     if not stat then
-      error(
-        string.format(
-          'The coroutine failed with this message: %s\n%s',
-          nargs_or_err,
-          debug.traceback(thread)
-        )
-      )
+      local co_err = ret[2] --- @type string
+      local err = debug.traceback(co, string.format('The async coroutine failed: %s', co_err))
+      if protected then
+        --- @cast callback -nil
+        callback(false, err)
+      else
+        error(err)
+      end
     end
 
-    if coroutine.status(thread) == 'dead' then
+    if coroutine.status(co) == 'dead' then
       if callback then
-        callback(unpack(ret, 2, table.maxn(ret)))
+        -- Include status if protected
+        callback(unpack(ret, protected and 1 or 2, table.maxn(ret)))
       end
       return
     end
 
-    --- @type function, any[]
-    local fn, args = ret[3], { unpack(ret, 4, table.maxn(ret)) }
-    args[nargs_or_err] = step
-    fn(unpack(args, 1, nargs_or_err))
+    --- @type integer, fun(...: any): any
+    local nargs, fn = ret[2], ret[3]
+
+    assert(type(fn) == 'function', 'type error :: expected func')
+
+    --- @type any[]
+    local args = { unpack(ret, 4, table.maxn(ret)) }
+
+    args[nargs] = step
+    if protected then
+      --- @cast callback -nil
+      xpcall(fn, function(err)
+        callback(false, ('The wrapped function failed: %s'):format(err))
+      end, unpack(args, 1, nargs))
+    else
+      fn(unpack(args, 1, nargs))
+    end
   end
 
   step(...)
+end
+
+--- @param argc integer
+--- @param func function
+--- @param ... any
+--- @return any ...
+function M.wait(argc, func, ...)
+  -- Always run the wrapped functions in xpcall and re-raise the error in the
+  -- coroutine. This makes pcall work as normal.
+  local function pfunc(...)
+    local args = { ... } --- @type any[]
+    local cb = args[argc]
+    args[argc] = function(...)
+      cb(true, ...)
+    end
+    xpcall(func, function(err)
+      cb(false, err, debug.traceback())
+    end, unpack(args, 1, argc))
+  end
+
+  local ret = { coroutine.yield(argc, pfunc, ...) }
+
+  local ok = ret[1]
+  if not ok then
+    --- @type string, string
+    local err, traceback = ret[2], ret[3]
+    error(string.format('Wrapped function failed: %s\n%s', err, traceback))
+  end
+
+  return unpack(ret, 2, table.maxn(ret))
 end
 
 --- Creates an async function with a callback style function.
@@ -44,29 +93,46 @@ end
 --- @param func F
 --- @return F
 function M.wrap(argc, func)
-  vim.validate({
-    func = { func, 'function' },
-    argc = { argc, 'number' },
-  })
-  --- @param ... unknown
-  --- @return unknown
+  assert(type(func) == 'function')
+  assert(type(argc) == 'number')
   return function(...)
-    return coroutine.yield(argc, func, ...)
+    return M.wait(argc, func, ...)
   end
 end
 
+--- create([argc, ] func)
+---
 --- Use this to create a function which executes in an async context but
 --- called from a non-async context. Inherently this cannot return anything
 --- since it is non-blocking
+---
+--- If argc is not provided, then the created async function cannot be continued
+---
 --- @generic F: function
---- @param nargs integer
---- @param func async F
+--- @param argc_or_func F|integer
+--- @param func? F
 --- @return F
-function M.create(nargs, func)
-  return function(...)
-    local callback = select(nargs + 1, ...)
-    execute(func, callback, unpack({ ... }, 1, nargs))
+function M.create(argc_or_func, func)
+  local argc --- @type integer
+  if type(argc_or_func) == 'function' then
+    assert(not func)
+    func = argc_or_func
+  elseif type(argc_or_func) == 'number' then
+    assert(type(func) == 'function')
+    argc = argc_or_func
   end
+
+  --- @cast func function
+
+  return function(...)
+    local callback = argc and select(argc + 1, ...) or nil
+    assert(not callback or type(callback) == 'function')
+    return run(func, false, callback, unpack({ ... }, 1, argc))
+  end
+end
+
+function M.run(func, ...)
+  return run(func, false, nil, ...)
 end
 
 --- Use this to create a function which executes in an async context but
@@ -77,7 +143,7 @@ end
 function M.sync(func, timeout)
   local done = false
   local ret --- @type table
-  execute(func, function(...)
+  run(func, false, function(...)
     done = true
     ret = { n = select('#', ...), ... }
   end)
